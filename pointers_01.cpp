4610fdb06d81a9efc98211a18bd2405d8650be39
Your Name <harshraskar775@gmail.com>
2023-07-05 23:31:54 +0530

    Initial Commit

---
+++ pointers_01.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    // int a = 5;
+
+    // cout<<a<<endl;
+    
+    // cout<<&a<<endl;
+
+    // poiner creation
+    // int b=3;
+    // int* harsh= &b;
+    // cout<<b;
+
+    // cout<<"address of harsh is"<<&harsh<<endl;
+    // cout<<"address of b is"<<&b<<endl;
+    // cout<<"value at b"<<*harsh<<endl;
+
+    // int a=5; 
+    // int* p=&a;
+    // cout<< sizeof(p)<<endl;
+
+    // char ch=a;
+    // char* d=&ch;
+    // cout<< sizeof(d)<<endl;
+
+    // null pointer
+    // int ptr=0;
+    // int* harsh=&ptr;
+    // cout<<&ptr<<endl;
+    
+
+    // COPY pointer
+    int dada=90;
+    int* h = &dada;
+    int* r= h;
+    cout<<*h<<endl;
+    return 0;
+}

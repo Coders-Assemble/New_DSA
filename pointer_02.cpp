8ae235eb1d2388da21acbad80cfea961920384f0
Your Name <harshraskar775@gmail.com>
2023-07-06 11:37:42 +0530

    Pointer-2 file added

---
+++ pointer_02.cpp
@@ -0,0 +1,23 @@
+# include<iostream>
+
+using namespace std;
+
+int main(){
+    // int arr[4]={21,23,34,45};
+    // cout<<arr<<endl;
+    // cout<<&arr<<endl;
+    // cout<<&arr[0]<<endl;
+    // cout<<arr[3]<<endl<<endl;
+
+
+    // int *p=arr;
+    // cout<<p<<endl;
+    // cout<<*p<<endl;
+
+
+    // character array
+    char ch[10]="babbar";
+    char*c = ch;
+    cout<<ch[2]<<endl;
+    return 0;
+}

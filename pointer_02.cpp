9a4eb48f172ce0037377368c47c79b3003a3d486
Your Name <harshraskar775@gmail.com>
2023-07-08 20:16:03 +0530

    pointer_03

--- pointer_02.cpp
+++ pointer_02.cpp
@@ -1,24 +1,43 @@
-# include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
+void solve(int *p){
+
+    // *Ptr = *p + 10;
+}
+
+int main()
+{
     // int arr[4]={21,23,34,45};
     // cout<<arr<<endl;
     // cout<<&arr<<endl;
     // cout<<&arr[0]<<endl;
     // cout<<arr[3]<<endl<<endl;
 
-
     // int *p=arr;
     // cout<<p<<endl;
     // cout<<*p<<endl;
 
-
     // character array
-    char ch[10]="babbar";
-    char*c = ch;
-    cout<<ch[2]<<endl;
-    cout<<endl;
+    // char ch[10]="babbar";
+    // char*c = ch;
+    // cout<<ch[2]<<endl;
+    // cout<<endl;
+
+    // char name[10]="sherbano";
+    // char* c= name;
+    // cout<<name<<endl;
+    // cout<<*name+1<<endl;
+    // cout<<c+1<<endl;
+    // cout<<c+3<<endl;
+    // cout<<*c<<endl;
+    // cout<<&c<<endl;
+
+    int a = 5;
+    int *ptr = &a;
+    solve(ptr);
+    cout << a;
+
     return 0;
 }
---
+++ pointer_03.cpp
@@ -0,0 +1,79 @@
+# include<iostream>
+
+using namespace std;
+
+void changeadd(int&p){
+
+        p=p+10;
+
+    }
+
+
+void solve(int** xyz){
+
+    // //  **xyz=*xyz+1;           //<<<gives you error
+    **xyz=**xyz+1;
+     *xyz=*xyz+1;
+     xyz=xyz+1;
+}
+
+void byrefernce(int& num){
+    num=30;    
+}
+
+void byrefernce_2(int* hello){
+    *hello=*hello +1;
+    }
+int main(){
+    // int a=5;
+    // int* p=&a;
+    // int** q=&p;
+    // cout<<p<<endl;
+    // cout<<&a<<endl;
+
+// using the function change in address
+    
+    // int a=5;
+    
+    // int* p=&a;
+
+
+    // cout<<"before"<<endl;
+    // cout<<a<<endl;
+    // cout<<p<<endl;
+    // cout<<*p<<endl;
+
+    // changeadd(a);
+
+    // cout<<"after"<<endl;
+    // cout<<a<<endl;
+    // cout<<p<<endl;
+    // cout<<*p<<endl;
+
+//////doing various functions in that changing the value of the input first iteger
+
+
+    // int a=5;
+    
+    // int* p=&a;
+    // int** q=&p;
+
+    // solve(q);
+
+    // cout<<a<<endl;
+
+
+    // pass by reference<<<<<<<<<
+
+    // int a=22;
+    // byrefernce(a);
+    // cout<<a<<endl;
+    // cout<<a<<endl;
+
+    int a=200;
+    byrefernce_2(&a);
+
+
+    cout<<a<<endl;
+    return 0;
+}
